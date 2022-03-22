955bb10de2739430530050d5ce7dfd2d2cb78471
sujon554 <mr.sujon554@gmail.com>
2022-03-22 07:10:42 +0600

    order

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const ObjectId = require("mongodb").ObjectId;
 const app = express();
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +27,7 @@ async function run () {
         console.log("success connect database");
         const database = client.db("Healthcare");
         const doctorsCollection = database.collection("doctors");
+        const orderCollection = database.collection("order");
 
 
 
@@ -48,7 +49,6 @@ async function run () {
     // Single Doctors details
     app.get("/doctors/:id" , async(req, res) => {
         const id = req.params.id;
-      console.log("Single Doctors", id);
       const query = { _id: ObjectId(id) };
       const result = await doctorsCollection.findOne(query);
       res.json(result);
